ea521fead00b4d1c1a8cd9600f95bfe2321bde19
BagleWagle <bzijer@gmail.com>
2024-11-25 21:16:40 +0300

    Работа над ошибками

--- 2 Module/module_2_3.py
+++ 2 Module/module_2_3.py
@@ -1,11 +1,10 @@
-
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 x = 0
-if my_list[x] >= 0 and x <= len(my_list):
-    while my_list[x] >= 0:
+if my_list[x] >= 0:
+    while my_list[x] >= 0 and x <= len(my_list):
         print(my_list[x])
         x = x+1
-        y = my_list[x]
+
 
 
 """
