000e96b4dd9195a6ded7f82b01815cef7ac88c38
BagleWagle <bzijer@gmail.com>
2024-12-04 22:39:52 +0300

    Домашняя работа по уроку "Модули и пакеты"

---
+++ 4 Module/module_4_1/fake_math.py
@@ -0,0 +1,6 @@
+def divide_error(first, second):
+    if second == 0:
+        return 'Error'
+    else:
+        sumfake = first / second
+        return sumfake
---
+++ 4 Module/module_4_1/true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+def divide_infiniti(first, second):
+    if second == 0:
+        return inf
+    else:
+        sumtrue = first / second
+        return sumtrue
