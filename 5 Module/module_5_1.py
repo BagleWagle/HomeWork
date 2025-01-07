843a589b94d85cc3f603f9eda43714ddae1ba849
BagleWagle <bzijer@gmail.com>
2025-01-07 03:19:25 +0300

    Домашняя работа по уроку "Специальные методы классов"

--- 5 Module/module_5_1.py
+++ 5 Module/module_5_1.py
@@ -9,13 +9,24 @@ class House:
             for i in range(0, new_floor):
                     print(i + 1)
         else:
-            print("Такого этажа не существует")
+            print('Такого этажа не существует!')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(5)
 h2.go_to(10)
 
+print(len(h1))
+print(len(h2))
+
+print(h1)
+print(h2)
 
 """
 Задача "Developer - не только разработчик":
